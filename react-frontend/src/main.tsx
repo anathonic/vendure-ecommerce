b4901c199b9f57a7256723059783524011c5f5b6
Anath <anathonic@gmail.com>
2025-05-15 22:54:40 +0200

    add tailwind css, graphql queries and apollo client

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/css/tailwind.css'
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './graphql/apolloClient.ts'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <ApolloProvider client={apolloClient}>
+      <App />
+  </ApolloProvider>
   </StrictMode>,
 )
